6949209839c23267d1830f17ec1a5585158c9cca
Deezi <deezitheviper@gmail.com>
2024-11-14 16:20:45 +0100

    pointer receiver updates

--- pointer_receivers.go
+++ pointer_receivers.go
@@ -9,11 +9,11 @@ type Vertex struct {
 	X, Y float64
 }
 
-func (v Vertex) Abs() float64 {
+func (v Vertex) Abst() float64 {
 	return math.Sqrt(v.X*v.X + v.Y*v.Y)
 }
 
-func (v *Vertex) Scale(f float64) { // The Scale method must have a pointer receiver to change the Vertex value declared in the main function.
+func (v *Vertex) Scalex(f float64) { // The Scale method must have a pointer receiver to change the Vertex value declared in the main function.
 	v.X = v.X * f
 	v.Y = v.Y * f
 }
@@ -27,8 +27,8 @@ func ScaleF(v *Vertex, f float64) { //The function Scale takes a pointer to a Ve
 
 func main() {
 	v := Vertex{3, 4}
-	v.Scale(10)
-	v.ScaleF(&V, 10) //By passing the address of v, you allow the Scale function to directly modify the original Vertex struct. If you passed v by value (without the &), a copy of v would be passed to Scale, and the changes made inside the function would not affect the original
+	v.Scalex(10)
+	ScaleF(&v, 10) //By passing the address of v, you allow the Scale function to directly modify the original Vertex struct. If you passed v by value (without the &), a copy of v would be passed to Scale, and the changes made inside the function would not affect the original
 	fmt.Println(v.Abs())
 
 	p := &Vertex{4, 3}
